9dc52040efcc8b8c4a7052396598feac4a08d24d
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-12 10:51:04 +0600

    fix the second.py second input by using float operator

--- second.py
+++ second.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter the first number : "))
 operator = str(input("Select any operation, =,-,*,/ : "))
-num2 = int(input("Enter the second number : "))
+num2 = float(input("Enter the second number : "))
 
 if operator == "+" : print("Result :",num1+num2)
 elif operator == "-" : print("Result :",num1-num2)
